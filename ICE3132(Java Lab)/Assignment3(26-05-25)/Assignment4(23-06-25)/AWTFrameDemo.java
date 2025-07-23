f7c7862689bce3a2fa3173ef1dbb38a48df2da76
dwip-nandi <dwipnandi100@gmail.com>
2025-07-23 23:57:45 +0600

    Some files are duplicated

--- ICE3132(Java Lab)/Assignment3(26-05-25)/Assignment4(23-06-25)/AWTFrameDemo.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-public class AWTFrameDemo {
-    public static void main(String[] args) {
-        Frame f = new Frame("My First GUI Frame");
-        f.setSize(400, 300);         // optional: set width and height
-        f.setVisible(true);          // explicitly make frame visible
-    }
-}
